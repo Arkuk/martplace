de8074e441876d904dc7be410b05801cd285a5cf
Arkuk <kukobinartur@yandex.ru>
2019-12-03 17:16:18 +0300

    added products section

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,25 @@ $(function () {
     readOnly: true
   });
 
+  $(".rate-product").rateYo({
+    starWidth: "12px",
+    rating: 4.5,
+    readOnly: true
+  });
+  
+
   $('.weklyProduct__slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     prevArrow: '<button type="button" class="slick-prev" ><span class="lnr lnr-chevron-left"></span></button>',
     nextArrow: '<button type="button" class="slick-next" ><span class="lnr lnr-chevron-right"></span></button>'
   });
+
+
+
+
+
+
+  var mixer = mixitup('.products__items');
 
 });
